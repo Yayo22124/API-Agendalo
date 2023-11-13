c990012b60d10d17c41978bdd9c19aca95ac0dce
LOctavioDev <octaviodevtech@gmail.com>
2023-11-13 14:24:20 -0600

    Add: Chalk

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import app from "./server.js"
+import chalk from 'chalk';
+
 
 app.listen(app.get('PORT'), () => {
-    console.log(`-------------------------------------------- \n
-        Server Listen on PORT: ${app.get('PORT')}
-    \n--------------------------------------------`);
-})
+    console.log(chalk.green('================================================='));
+    console.log(chalk.green('Conexión a la base de datos establecida con éxito'));
+    console.log(chalk.green('================================================='));
+})  
